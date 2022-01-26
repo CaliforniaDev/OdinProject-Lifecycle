8340275f6f54cc60abdbc0fcc40039f33d780d33
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-01-26 02:17:54 -0800

    Import counter component to parent

--- lifecycle-assignment/src/App.js
+++ lifecycle-assignment/src/App.js
@@ -1,10 +1,19 @@
-
-const App = () => {
-  return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
-  );
+import React from "react";
+import Counter from "./components/Counter";
+// LIFECYCLE METHODS //
+// MOUNTING, UPDATING, ERROR BOUNDARIES, UNMOUNTING///
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Counter />
+      </div>
+    )
+  }
 }
+
+
+
+
 
 export default App;
---
+++ lifecycle-assignment/src/components/Counter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('Contructor');
+ 
+    this.state= {
+      counter: 0,
+    }
+  }
+  increment = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      counter: prevState.counter + 1
+    }));
+  }
+  decrement = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      counter: prevState.counter - 1
+    }))
+  }
+  render() {
+    console.log('render');
+    const { counter } = this.state
+    return (
+      <div className="counter">
+        <h1>Counter: {counter}</h1>
+        <button onClick={this.increment}>INCREMENT</button>
+        <button onClick={this.decrement}>DECREMENT</button>
+      </div>
+    )
+  }
+}
+export default Counter;
