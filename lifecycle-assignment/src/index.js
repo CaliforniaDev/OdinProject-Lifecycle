7e57684c72efb606c95abf457be872e2d5ba1d73
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-04 19:50:59 -0800

    Test create root api

--- lifecycle-assignment/src/index.js
+++ lifecycle-assignment/src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from './theme/GlobalStyle'
 import reportWebVitals from './reportWebVitals';
-
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+  
+);
+/*
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
@@ -11,7 +20,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
